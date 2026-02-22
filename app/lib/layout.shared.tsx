715b4aa40c67fb2d20656fc22b289c5c622fa5de
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-02-22 20:28:05 +0900

    First commit

---
+++ app/lib/layout.shared.tsx
@@ -0,0 +1,15 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+// fill this with your actual GitHub info, for example:
+export const gitConfig = {
+    user: 'fuma-nama',
+    repo: 'fumadocs',
+    branch: 'main',
+};
+export function baseOptions(): BaseLayoutProps {
+    return {
+        nav: {
+            title: 'React Router',
+        },
+    };
+}
