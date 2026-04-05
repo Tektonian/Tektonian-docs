19024c0ffa759ce8d0fb228972ca64d1c1002970
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-04-05 10:17:58 +0900

    chore: removed needless analysis components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,3 @@
-import { AnalyticsProvider, resolveAnalyticsEvent, useAnalytics } from "./lib/analytics";
 import { cn } from "./lib/utils";
 
-export { AnalyticsProvider, cn, resolveAnalyticsEvent, useAnalytics };
-export type { AnalyticsCapture, AnalyticsProperties, TrackProps } from "./lib/analytics";
+export { cn }
